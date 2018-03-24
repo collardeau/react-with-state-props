05cb43a5e37a81f8d7558e292048fb945ac5e5d7
Thomas Collardeau <thomas@collardeau.com>
2018-03-24 14:02:50 +0100

    reduce withState props

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,5 +1,55 @@
-import add from "../src/index";
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+import Container from "../src/index";
 
-it("passes a basic test", () => {
-  expect(add(2, 4)).toBe(6);
+function renderComp(props) {
+  return renderer.create(
+    <Container {...props} render={renderProps => <div {...renderProps} />} />
+  );
+}
+
+function getProps(props) {
+  const comp = renderComp(props);
+  const tree = comp.toJSON();
+  return tree && tree.props;
+}
+
+test("create some state", () => {
+  const withState = [
+    {
+      name: "counter",
+      init: 0
+    },
+    {
+      name: "loaded",
+      init: false
+    }
+  ];
+  const comp = renderComp({ withState });
+  let tree = comp.toJSON();
+  expect(tree.props.loaded).toBe(false);
+  expect(tree.props.counter).toBe(0);
+});
+
+test("create some simple actions", () => {
+  const withState = [
+    {
+      name: "counter",
+      init: 0
+    }
+  ];
+  const comp = renderComp({ withState });
+  let tree = comp.toJSON();
+  console.log(tree.props);
+  expect(tree.props.counter).toBe(0);
+  expect(tree.props.setCounter).toBeDefined();
+  tree.props.setCounter(10);
+  tree = comp.toJSON();
+  expect(tree.props.counter).toBe(10);
+  // check setState callback
+  const spy = jest.fn();
+  tree.props.setCounter(20, spy);
+  tree = comp.toJSON();
+  expect(tree.props.counter).toBe(20);
+  expect(spy).toBeCalled();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,58 @@
-console.log("Hello, Typescript");
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import * as R from "ramda";
 
-export default function add(x: number, y: number) {
-  return x + y;
+// types
+
+interface State {
+  [name: string]: any;
+}
+
+interface StateDef {
+  name: string;
+  init: State;
+}
+
+interface Props {
+  render: (props: object) => JSX.Element;
+  withState: StateDef[];
+}
+
+// functions
+
+const noop = () => {};
+const cap = (string: string) =>
+  string.charAt(0).toUpperCase() + string.slice(1);
+
+const setHandler = (comp: any, name: string) => ({
+  [`set${cap(name)}`]: (val: State, cb: Function = noop) => {
+    comp.setState(
+      {
+        [name]: val
+      },
+      cb
+    );
+  }
+});
+
+const reduceStates = R.curry((comp: any, stateDefs: StateDef[]) =>
+  R.reduce(
+    (acc, { name, init }: StateDef) =>
+      R.mergeAll([acc, { [name]: init }, setHandler(comp, name)]),
+    {}
+  )(stateDefs)
+);
+
+// React
+
+export default class Container extends React.Component<Props, {}> {
+  state = {};
+  componentWillMount() {
+    R.pipe(reduceStates(this), R.tap(state => this.setState(state)))(
+      this.props.withState
+    );
+  }
+  render() {
+    return this.props.render({ ...this.state });
+  }
 }
